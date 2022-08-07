c9a4f2ca35a63010fd952dee5655ced352ad699b
Neeraj Malkani <neerajmalkani33@gmail.com>
2022-08-08 02:09:12 +0530

    Material Setup in progress

---
+++ src/screens/Admin/ServiceCatalogue/AddItems/AddMaterialSetupProducts.js
@@ -0,0 +1,188 @@
+import React, { useEffect, useRef } from "react";
+import { View } from "react-native";
+import { Checkbox, Title } from "react-native-paper";
+import Provider from "../../../../api/Provider";
+import Dropdown from "../../../../components/Dropdown";
+import { Styles } from "../../../../styles/styles";
+
+const AddMaterialSetupProducts = ({ arrProductData }) => {
+  const [activityFullData, setActivityFullData] = React.useState([]);
+
+  const [servicesFullData, setServicesFullData] = React.useState([]);
+  const [servicesData, setServicesData] = React.useState([]);
+  const [serviceName, setServiceName] = React.useState("");
+  const servicesDDRef = useRef({});
+
+  const [categoriesFullData, setCategoriesFullData] = React.useState([]);
+  const [categoriesData, setCategoriesData] = React.useState([]);
+  const [categoriesName, setCategoriesName] = React.useState("");
+  const categoriesDDRef = useRef({});
+
+  const [productsFullData, setProductsFullData] = React.useState([]);
+
+  const FetchActvityRoles = () => {
+    Provider.getAll("master/getmainactivities")
+      .then((response) => {
+        if (response.data && response.data.code === 200) {
+          if (response.data.data) {
+            response.data.data = response.data.data.filter((el) => {
+              return el.display && el.activityRoleName === "Dealer";
+            });
+            setActivityFullData(response.data.data);
+            servicesDDRef.current.reset();
+            setServiceName("");
+            setCategoriesName("");
+            setCategoriesData([]);
+            setServicesData([]);
+            setProductsFullData([]);
+            FetchServicesFromActivity("Dealer", response.data.data);
+          }
+        }
+      })
+      .catch((e) => {});
+  };
+
+  const FetchServicesFromActivity = (selectedItem, activityData) => {
+    let params = {
+      ID: activityData.find((el) => {
+        return el.activityRoleName === selectedItem;
+      }).id,
+    };
+    Provider.getAll(`master/getservicesbyroleid?${new URLSearchParams(params)}`)
+      .then((response) => {
+        if (response.data && response.data.code === 200) {
+          if (response.data.data) {
+            response.data.data = response.data.data.filter((el) => {
+              return el.display;
+            });
+            setServicesFullData(response.data.data);
+            const services = response.data.data.map((data) => data.serviceName);
+            setServicesData(services);
+          }
+        }
+      })
+      .catch((e) => {});
+  };
+
+  const FetchCategoriesFromServices = (selectedItem) => {
+    let params = {
+      ActivityID: activityFullData.find((el) => {
+        return el.activityRoleName === "Dealer";
+      }).id,
+      ServiceID: servicesFullData.find((el) => {
+        return el.serviceName === selectedItem;
+      }).id,
+    };
+    Provider.getAll(`master/getcategoriesbyserviceid?${new URLSearchParams(params)}`)
+      .then((response) => {
+        if (response.data && response.data.code === 200) {
+          if (response.data.data) {
+            response.data.data = response.data.data.filter((el) => {
+              return el.display;
+            });
+            setCategoriesFullData(response.data.data);
+            const categories = response.data.data.map((data) => data.categoryName);
+            setCategoriesData(categories);
+          }
+        }
+      })
+      .catch((e) => {});
+  };
+
+  const FetchProductsFromCategory = (selectedItem) => {
+    let params = {
+      ActivityID: activityFullData.find((el) => {
+        return el.activityRoleName === "Dealer";
+      }).id,
+      ServiceID: servicesFullData.find((el) => {
+        return el.serviceName === serviceName;
+      }).id,
+      CategoryID: categoriesFullData.find((el) => {
+        return el.categoryName === selectedItem;
+      }).id,
+    };
+    Provider.getAll(`master/getproductsbycategoryid?${new URLSearchParams(params)}`)
+      .then((response) => {
+        if (response.data && response.data.code === 200) {
+          if (response.data.data) {
+            response.data.data = response.data.data.filter((el) => {
+              return el.display;
+            });
+            const fullData = response.data.data.map((o) => ({
+              ...o,
+              isChecked: arrProductData[0].find((el) => {
+                return el.productID === o.productID;
+              })
+                ? true
+                : false,
+            }));
+            setProductsFullData(fullData);
+          }
+        }
+      })
+      .catch((e) => {});
+  };
+
+  useEffect(() => {
+    FetchActvityRoles();
+  }, []);
+
+  const onServiceNameSelected = (selectedItem) => {
+    setServiceName(selectedItem);
+    categoriesDDRef.current.reset();
+    setCategoriesData([]);
+    setProductsFullData([]);
+    setCategoriesName("");
+    FetchCategoriesFromServices(selectedItem);
+  };
+
+  const onCategoriesNameSelected = (selectedItem) => {
+    setCategoriesName(selectedItem);
+    setProductsFullData([]);
+    FetchProductsFromCategory(selectedItem);
+  };
+
+  return (
+    <View>
+      <View style={[Styles.flexRow, Styles.padding16]}>
+        <View style={[Styles.paddingStart0, Styles.paddingEnd8, Styles.flex1]}>
+          <Dropdown label="Service Name" data={servicesData} onSelected={onServiceNameSelected} selectedItem={serviceName} reference={servicesDDRef} />
+        </View>
+        <View style={[Styles.paddingStart8, Styles.paddingEnd0, Styles.flex1]}>
+          <Dropdown label="Category Name" data={categoriesData} onSelected={onCategoriesNameSelected} selectedItem={categoriesName} reference={categoriesDDRef} />
+        </View>
+      </View>
+      <View style={[Styles.padding16]}>
+        {productsFullData.length > 0 ? <Title style={[Styles.paddingHorizontal16, Styles.paddingBottom16, Styles.borderBottom1]}>Products</Title> : null}
+        {productsFullData.map((k, i) => {
+          return (
+            <Checkbox.Item
+              key={i}
+              status={k.isChecked ? "checked" : "unchecked"}
+              style={[Styles.borderBottom1]}
+              label={k.productName}
+              onPress={() => {
+                const tempArrProductData = [...arrProductData[0]];
+                if (!k.isChecked) {
+                  tempArrProductData.push(k);
+                } else {
+                  tempArrProductData.splice(tempArrProductData.indexOf(k), 1);
+                }
+                arrProductData[1](tempArrProductData);
+                let temp = productsFullData.map((u) => {
+                  if (k.productID === u.productID) {
+                    return { ...u, isChecked: !u.isChecked };
+                  }
+                  return u;
+                });
+                setProductsFullData(temp);
+              }}
+            />
+          );
+        })}
+      </View>
+    </View>
+  );
+};
+
+export default AddMaterialSetupProducts;
--- src/screens/Admin/ServiceCatalogue/AddItems/AddMaterialSetupScreen.js
+++ src/screens/Admin/ServiceCatalogue/AddItems/AddMaterialSetupScreen.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useRef } from "react";
-import { ScrollView, View } from "react-native";
-import { HelperText } from "react-native-paper";
+import { ScrollView, View, Dimensions } from "react-native";
+import { Button, Card, Checkbox, DataTable, HelperText, Snackbar, Subheading, Text, TextInput, Title } from "react-native-paper";
+import RBSheet from "react-native-raw-bottom-sheet";
 import Provider from "../../../../api/Provider";
 import Dropdown from "../../../../components/Dropdown";
 import { Styles } from "../../../../styles/styles";
+import { theme } from "../../../../theme/apptheme";
 import { communication } from "../../../../utils/communication";
+import AddMaterialSetupProducts from "./AddMaterialSetupProducts";
 
 const AddMaterialSetupScreen = ({ route, navigation }) => {
+  const arrProductData = React.useState([]);
+
   const [activityFullData, setActivityFullData] = React.useState([]);
 
   const [servicesFullData, setServicesFullData] = React.useState([]);
@@ -33,8 +38,21 @@ const AddMaterialSetupScreen = ({ route, navigation }) => {
   const [errorDT, setDTError] = React.useState(false);
   const designTypeDDRef = useRef({});
 
+  const [checked, setChecked] = React.useState(route.params.type === "edit" ? route.params.data.display : false);
+
+  const [lengthFeet, setLengthFeet] = React.useState(route.params.type === "edit" ? route.params.data.lengthFeet : "1");
+  const [lengthInches, setLengthInches] = React.useState(route.params.type === "edit" ? route.params.data.lengthInches : "0");
+
+  const [widthFeet, setWidthFeet] = React.useState(route.params.type === "edit" ? route.params.data.widthFeet : "1");
+  const [widthInches, setWidthInches] = React.useState(route.params.type === "edit" ? route.params.data.widthInches : "0");
+
   const [snackbarVisible, setSnackbarVisible] = React.useState(false);
   const [snackbarText, setSnackbarText] = React.useState("");
+
+  const windowHeight = Dimensions.get("window").height;
+  const refRBSheet = useRef();
+
+  const [showView, setShowView] = React.useState(1);
 
   const FetchActvityRoles = () => {
     Provider.getAll("master/getmainactivities")
@@ -45,7 +63,7 @@ const AddMaterialSetupScreen = ({ route, navigation }) => {
               return el.display && el.activityRoleName === "Contractor";
             });
             setActivityFullData(response.data.data);
-            
+
             if (route.params.type !== "edit") {
               servicesDDRef.current.reset();
               setServiceName("");
@@ -161,15 +179,8 @@ const AddMaterialSetupScreen = ({ route, navigation }) => {
       .catch((e) => {});
   };
 
-  const FetchDesignTypeFromProduct = (selectedItem, activityData) => {
+  const FetchDesignTypeFromProduct = (selectedItem) => {
     let params = {
-      ActivityID: activityData
-        ? activityData.find((el) => {
-            return el.activityRoleName === "Contractor";
-          }).id
-        : activityFullData.find((el) => {
-            return el.activityRoleName === "Contractor";
-          }).id,
       ServiceID:
         route.params.type === "edit"
           ? route.params.data.serviceID
@@ -180,16 +191,16 @@ const AddMaterialSetupScreen = ({ route, navigation }) => {
         route.params.type === "edit"
           ? route.params.data.categoryID
           : categoriesFullData.find((el) => {
-              return el.categoryName === selectedItem;
+              return el.categoryName === categoriesName;
             }).id,
       ProductID:
         route.params.type === "edit"
           ? route.params.data.productID
           : productsFullData.find((el) => {
               return el.productName === selectedItem;
-            }).id,
+            }).productID,
     };
-    Provider.getAll(`master/getdesigntypebyproductid?${new URLSearchParams(params)}`)
+    Provider.getAll(`servicecatalogue/getdesigntypebyproductid?${new URLSearchParams(params)}`)
       .then((response) => {
         if (response.data && response.data.code === 200) {
           if (response.data.data) {
@@ -253,6 +264,7 @@ const AddMaterialSetupScreen = ({ route, navigation }) => {
       }).id;
     }
     setPNError(false);
+    FetchDesignTypeFromProduct(selectedItem);
   };
 
   const onDesignTypeSelected = (selectedItem) => {
@@ -263,6 +275,42 @@ const AddMaterialSetupScreen = ({ route, navigation }) => {
       }).id;
     }
     setDTError(false);
+  };
+
+  const onLengthFeetSelected = (selectedItem) => {
+    setLengthFeet(selectedItem);
+  };
+
+  const onLengthInchesSelected = (selectedItem) => {
+    setLengthInches(selectedItem);
+  };
+
+  const onWidthFeetSelected = (selectedItem) => {
+    setWidthFeet(selectedItem);
+  };
+
+  const onWidthInchesSelected = (selectedItem) => {
+    setWidthInches(selectedItem);
+  };
+
+  const ValidateData = () => {};
+
+  const OpenProductDialog = () => {
+    refRBSheet.current.open();
+    setShowView(1);
+  };
+
+  const OpenProductViewDialog = () => {
+    refRBSheet.current.open();
+    setShowView(2);
+  };
+
+  const CreateNumberDropdown = (startCount, endCount) => {
+    let arrNumbers = [];
+    for (var i = startCount; i <= endCount; i++) {
+      arrNumbers.push(i.toString());
+    }
+    return arrNumbers;
   };
 
   return (
@@ -285,8 +333,95 @@ const AddMaterialSetupScreen = ({ route, navigation }) => {
           <HelperText type="error" visible={errorDT}>
             {communication.InvalidDesignTypeName}
           </HelperText>
+          <View style={{ width: 160 }}>
+            <Checkbox.Item label="Display" position="leading" style={{ paddingHorizontal: 2 }} labelStyle={{ textAlign: "left", paddingLeft: 8 }} color={theme.colors.primary} status={checked ? "checked" : "unchecked"} onPress={() => setChecked(!checked)} />
+          </View>
+          <Subheading style={[Styles.marginTop16]}>Length</Subheading>
+          <View style={[Styles.flexRow, Styles.flexAlignCenter]}>
+            <View style={[Styles.paddingStart0, Styles.paddingEnd8, Styles.flex5]}>
+              <Dropdown label="Feet" data={CreateNumberDropdown(1, 50)} onSelected={onLengthFeetSelected} selectedItem={lengthFeet} />
+            </View>
+            <Text style={[Styles.flex1, Styles.paddingStart4]}>ft</Text>
+            <View style={[Styles.paddingStart8, Styles.paddingEnd0, Styles.flex5]}>
+              <Dropdown label="Inches" data={CreateNumberDropdown(0, 11)} onSelected={onLengthInchesSelected} selectedItem={lengthInches} />
+            </View>
+            <Text style={[Styles.flex1, Styles.paddingStart4]}>in</Text>
+          </View>
+          <Subheading style={[Styles.marginTop32]}>Width / Height</Subheading>
+          <View style={[Styles.flexRow, Styles.flexAlignCenter, Styles.marginBottom32]}>
+            <View style={[Styles.paddingStart0, Styles.paddingEnd8, Styles.flex5]}>
+              <Dropdown label="Feet" data={CreateNumberDropdown(1, 50)} onSelected={onWidthFeetSelected} selectedItem={widthFeet} />
+            </View>
+            <Text style={[Styles.flex1, Styles.paddingStart4]}>ft</Text>
+            <View style={[Styles.paddingStart8, Styles.paddingEnd0, Styles.flex5]}>
+              <Dropdown label="Inches" data={CreateNumberDropdown(0, 11)} onSelected={onWidthInchesSelected} selectedItem={widthInches} />
+            </View>
+            <Text style={[Styles.flex1, Styles.paddingStart4]}>in</Text>
+          </View>
+          <TextInput mode="flat" label="Total (Sq.Ft.)" value={(parseFloat(lengthFeet + "." + lengthInches) * parseFloat(widthFeet + "." + widthInches)).toFixed(4)} editable={false} />
         </View>
       </ScrollView>
+      <View style={[Styles.backgroundColor, Styles.width100per, Styles.marginTop32, Styles.padding16, { position: "absolute", bottom: 0, elevation: 3 }]}>
+        <Card.Content style={[Styles.flexRow, { justifyContent: "space-between" }]}>
+          <Button mode="outlined" onPress={OpenProductDialog}>
+            Add
+          </Button>
+          <Button mode="contained" onPress={ValidateData}>
+            Submit
+          </Button>
+          <Button mode="outlined" onPress={OpenProductViewDialog}>
+            Show
+          </Button>
+        </Card.Content>
+      </View>
+      <Snackbar visible={snackbarVisible} onDismiss={() => setSnackbarVisible(false)} duration={3000} style={{ backgroundColor: theme.colors.error }}>
+        {snackbarText}
+      </Snackbar>
+      <RBSheet ref={refRBSheet} closeOnDragDown={true} closeOnPressMask={true} dragFromTopOnly={true} height={windowHeight - 96} animationType="fade" customStyles={{ wrapper: { backgroundColor: "rgba(0,0,0,0.5)" } }}>
+        <View style={[Styles.flex1]}>
+          <ScrollView style={[Styles.flex1, Styles.backgroundColor, { marginBottom: 64 }]}>
+            <View style={{ display: showView === 1 ? "flex" : "none" }}>
+              <AddMaterialSetupProducts arrProductData={arrProductData} />
+            </View>
+            <View style={[Styles.padding16, { display: showView === 2 ? "flex" : "none" }]}>
+              {arrProductData[0].map((k, i) => {
+                return (
+                  <View key={i} style={[Styles.flexColumn, Styles.border1, Styles.marginTop16]}>
+                    <View style={[Styles.flexRow, Styles.borderBottom1, Styles.padding4, Styles.flexAlignCenter]}>
+                      <Title style={[Styles.flex1, Styles.primaryColor]}>{k.productName}</Title>
+                    </View>
+                    <View style={[Styles.flexRow, Styles.borderBottom1, Styles.padding4, Styles.flexAlignCenter]}>
+                      <Text style={[Styles.flex1]}>Brand Name</Text>
+                      <TextInput mode="flat" dense style={[Styles.flex1]} editable={false} />
+                    </View>
+                    <View style={[Styles.flexRow, Styles.borderBottom1, Styles.padding4, Styles.flexAlignCenter]}>
+                      <Text style={[Styles.flex1]}>Quantity</Text>
+                      <TextInput mode="flat" dense style={[Styles.flex1]} editable={false} />
+                    </View>
+                    <View style={[Styles.flexRow, Styles.borderBottom1, Styles.padding4, Styles.flexAlignCenter]}>
+                      <Text style={[Styles.flex1]}>Rate</Text>
+                      <TextInput mode="flat" dense style={[Styles.flex1, { backgroundColor: theme.colors.textLight }]} />
+                    </View>
+                    <View style={[Styles.flexRow, Styles.borderBottom1, Styles.padding4, Styles.flexAlignCenter]}>
+                      <Text style={[Styles.flex1]}>Amount</Text>
+                      <TextInput mode="flat" dense style={[Styles.flex1]} editable={false} />
+                    </View>
+                    <View style={[Styles.flexRow, Styles.padding4, Styles.flexAlignCenter]}>
+                      <Text style={[Styles.flex1]}>Formula</Text>
+                      <TextInput mode="flat" dense style={[Styles.flex1, { backgroundColor: theme.colors.textLight }]} />
+                    </View>
+                  </View>
+                );
+              })}
+            </View>
+          </ScrollView>
+          <View style={[Styles.backgroundColor, Styles.width100per, Styles.marginTop32, Styles.padding16, { position: "absolute", bottom: 0, elevation: 3 }]}>
+            <Button mode="contained" onPress={() => refRBSheet.current.close()}>
+              Done
+            </Button>
+          </View>
+        </View>
+      </RBSheet>
     </View>
   );
 };
