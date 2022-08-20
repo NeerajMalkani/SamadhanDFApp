53fda2fb53d3bdadcefeb2d6ec8523ec36c6f037
Centeos <107580733+Centeos@users.noreply.github.com>
2022-08-20 13:35:15 +0530

    Contractor - organization
    Contractor - organization

---
+++ src/screens/Contractor/Organization/BranchScreen.js
@@ -0,0 +1,161 @@
+import React, { useEffect } from "react";
+import { ActivityIndicator, View, LogBox, RefreshControl } from "react-native";
+import { FAB, List, Searchbar, Snackbar } from "react-native-paper";
+import { SwipeListView } from "react-native-swipe-list-view";
+import Provider from "../../../api/Provider";
+import Header from "../../../components/Header";
+import { RenderHiddenItems } from "../../../components/ListActions";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import NoItems from "../../../components/NoItems";
+import { Styles } from "../../../styles/styles";
+import { theme } from "../../../theme/apptheme";
+
+LogBox.ignoreLogs(["Non-serializable values were found in the navigation state"]);
+let ContractorID = 0;
+
+const BranchScreen = ({ navigation }) => {
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(true);
+  const listData = React.useState([]);
+  const listSearchData = React.useState([]);
+  const [refreshing, setRefreshing] = React.useState(false);
+  const [snackbarVisible, setSnackbarVisible] = React.useState(false);
+  const [snackbarText, setSnackbarText] = React.useState("");
+  const [snackbarColor, setSnackbarColor] = React.useState(theme.colors.success);
+
+  const GetUserID = async () => {
+    const userData = await AsyncStorage.getItem("user");
+    if (userData !== null) {
+        ContractorID = JSON.parse(userData).UserID;
+        FetchData();
+    }
+  };
+
+  useEffect(() => {
+    GetUserID();
+  }, []);
+
+  const FetchData = (from) => {
+    if (from === "add" || from === "update") {
+      setSnackbarText("Item " + (from === "add" ? "added" : "updated") + " successfully");
+      setSnackbarColor(theme.colors.success);
+      setSnackbarVisible(true);
+    }
+    let params = {
+        UserType: 3,
+        UserId: ContractorID
+      };
+    Provider.getAll(`master/getuserdesignation?${new URLSearchParams(params)}`)
+      .then((response) => {
+        debugger;
+        if (response.data && response.data.code === 200) {
+          if (response.data.data) {
+            const lisData = [...response.data.data];
+            lisData.map((k, i) => {
+              k.key = (parseInt(i) + 1).toString();
+            });
+            listData[1](response.data.data);
+            listSearchData[1](response.data.data);
+          }
+        } else {
+          listData[1]([]);
+          setSnackbarText("No data found");
+          setSnackbarColor(theme.colors.error);
+          setSnackbarVisible(true);
+        }
+        setIsLoading(false);
+        setRefreshing(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setSnackbarText(e.message);
+        setSnackbarColor(theme.colors.error);
+        setSnackbarVisible(true);
+        setRefreshing(false);
+      });
+  };
+
+  const onChangeSearch = (query) => {
+    setSearchQuery(query);
+    if (query === "") {
+      listSearchData[1](listData[0]);
+    } else {
+      listSearchData[1](
+        listData[0].filter((el) => {
+          return el.designationName.toString().toLowerCase().includes(query.toLowerCase());
+        })
+      );
+    }
+  };
+
+  const RenderItems = (data) => {
+    return (
+      <View style={[Styles.backgroundColor, Styles.borderBottom1, Styles.paddingStart16, Styles.flexJustifyCenter, { height: 72 }]}>
+        <List.Item title={data.item.designationName} titleStyle={{ fontSize: 18 }} description={"Display: " + (data.item.display ? "Yes" : "No")} left={() => <Icon style={{ marginVertical: 12, marginRight: 12 }} size={30} color={theme.colors.textSecondary} name="account" />} />
+      </View>
+    );
+  };
+
+  const AddCallback = () => {
+    navigation.navigate("AddDesignationScreen", { type: "add", fetchData: FetchData });
+  };
+
+  const EditCallback = (data, rowMap) => {
+    rowMap[data.item.key].closeRow();
+    navigation.navigate("AddDesignationScreen", {
+      type: "edit",
+      fetchData: FetchData,
+      data: {
+        id: data.item.id,
+        designationName: data.item.designationName,
+        display: data.item.display,
+        reportingAuthority: data.item.reportingAuthority,
+        uid: ContractorID
+      },
+    });
+  };
+
+  return (
+    <View style={[Styles.flex1]}>
+      <Header navigation={navigation} title="Designations" />
+      {isLoading ? (
+        <View style={[Styles.flex1, Styles.flexJustifyCenter, Styles.flexAlignCenter]}>
+          <ActivityIndicator size="large" color={theme.colors.primary} />
+        </View>
+      ) : listData[0].length > 0 ? (
+        <View style={[Styles.flex1, Styles.flexColumn, Styles.backgroundColor]}>
+          <Searchbar style={[Styles.margin16]} placeholder="Search" onChangeText={onChangeSearch} value={searchQuery} />
+          <SwipeListView
+            previewDuration={1000}
+            previewOpenValue={-72}
+            previewRowKey="1"
+            previewOpenDelay={1000}
+            refreshControl={
+              <RefreshControl
+                colors={[theme.colors.primary]}
+                refreshing={refreshing}
+                onRefresh={() => {
+                  FetchData();
+                }}
+              />
+            }
+            data={listSearchData[0]}
+            useFlatList={true}
+            disableRightSwipe={true}
+            rightOpenValue={-72}
+            renderItem={(data) => RenderItems(data)}
+            renderHiddenItem={(data, rowMap) => RenderHiddenItems(data, rowMap, [EditCallback])}
+          />
+        </View>
+      ) : (
+        <NoItems icon="format-list-bulleted" text="No records found. Add records by clicking on plus icon." />
+      )}
+      <FAB style={[Styles.margin16, Styles.primaryBgColor, { position: "absolute", right: 16, bottom: 16 }]} icon="plus" onPress={AddCallback} />
+      <Snackbar visible={snackbarVisible} onDismiss={() => setSnackbarVisible(false)} duration={3000} style={{ backgroundColor: snackbarColor }}>
+        {snackbarText}
+      </Snackbar>
+    </View>
+  );
+};
+
+export default BranchScreen;
