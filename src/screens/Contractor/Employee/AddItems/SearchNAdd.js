4d62ea2d047946e32f4d9d1c04ed181c05650f2b
Centeos <107580733+Centeos@users.noreply.github.com>
2022-09-13 22:18:54 +0530

    update search employee screen

--- src/screens/Contractor/Employee/AddItems/SearchNAdd.js
+++ src/screens/Contractor/Employee/AddItems/SearchNAdd.js
@@ -363,7 +363,9 @@ const [searchQuery, setSearchQuery] = React.useState("");
     <View style={[Styles.flex1]}>
         <ScrollView style={[Styles.flex1, Styles.backgroundColor, { marginBottom: 0 }]} keyboardShouldPersistTaps="handled">
             <View style={[Styles.padding16]}>
-
+            <View style={[Styles.width100per,Styles.borderBottom2,Styles.borderBottom2, Styles.marginBottom8]}>
+                    <Text style={[Styles.fontSize20,Styles.fontBold,Styles.marginBottom4,Styles.primaryColor]}>Search Employee</Text>
+                </View>
               <TextInput ref={aadharNoRef} mode="flat" dense label="Employee Aadhar No" value={aadharNo} returnKeyType="next" onSubmitEditing={() => aadharNoRef.current.focus()} onChangeText={onAadharNoChanged} style={{ backgroundColor: "white" }} error={aadharNoInvalid} />
               <HelperText type="error" visible={aadharNoInvalid}>
                 {communication.InvalidAadharNo}
@@ -426,7 +428,9 @@ const [searchQuery, setSearchQuery] = React.useState("");
                 <Text style={[Styles.fontSize20,Styles.SpaceEvenly,Styles.flexRow,Styles.textSecondaryColor]}>- - - - - - - - - - - - - - - OR - - - - - - - - - - - - - - - -</Text>
             </View>
             <View style={[Styles.padding16]}>
-
+            <View style={[Styles.width100per,Styles.borderBottom2,Styles.borderBottom2, Styles.marginBottom16]}>
+                    <Text style={[Styles.fontSize20,Styles.fontBold,Styles.marginBottom4,Styles.primaryColor]}>Add New Employee</Text>
+                </View>
               <TextInput ref={employeeNameRef} mode="flat" dense label="Employee Name" value={employeeName} returnKeyType="next" 
               onSubmitEditing={() => employeeNameRef.current.focus()} selectTextOnFocus={false} onChangeText={onEmployeeNameChanged} 
               style={{ backgroundColor: "white" }} error={employeeNameInvalid} />
