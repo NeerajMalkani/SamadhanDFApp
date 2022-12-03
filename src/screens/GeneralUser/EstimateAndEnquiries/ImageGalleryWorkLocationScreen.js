2119b89998e8e5b4a08b11fbc45969487f44ea4c
Centeos <107580733+Centeos@users.noreply.github.com>
2022-12-03 12:02:58 +0530

    home screen changes

--- src/screens/GeneralUser/EstimateAndEnquiries/ImageGalleryWorkLocationScreen.js
+++ src/screens/GeneralUser/EstimateAndEnquiries/ImageGalleryWorkLocationScreen.js
@@ -28,17 +28,25 @@ const ImageGalleryWorkLocationScreen = ({ route, navigation }) => {
     const userData = await AsyncStorage.getItem("user");
     if (userData !== null) {
       setUser(JSON.parse(userData));
+      FetchImageGalleryData(JSON.parse(userData).UserID, JSON.parse(userData).Sess_group_refno);
     }
   };
 
-  const FetchImageGalleryData = () => {
+  const FetchImageGalleryData = (UserID, Sess_group_refno) => {
     let params = {
-      CategoryID: route.params.categoryID,
+      data: {
+        Sess_UserRefno: UserID,
+        Sess_group_refno: Sess_group_refno,
+        service_refno: route.params.data.id
+      },
     };
-    Provider.getAll(`generaluserenquiryestimations/getimagegallerybycategoryid?${new URLSearchParams(params)}`)
+    console.log(params);
+    Provider.createDF(Provider.API_URLS.GetserviceimagegalleryByServicerefno, params)
       .then((response) => {
+        console.log(response.data);
         if (response.data && response.data.code === 200) {
           if (response.data.data) {
+            response.data.data = APIConverter(response.data.data);
             setImageGalleryData(response.data.data);
           }
         } else {
@@ -58,7 +66,7 @@ const ImageGalleryWorkLocationScreen = ({ route, navigation }) => {
   };
 
   useEffect(() => {
-    FetchImageGalleryData();
+    
     GetUserID();
     navigation.setOptions({ headerTitle: route.params.headerTitle });
   }, []);
@@ -72,6 +80,72 @@ const ImageGalleryWorkLocationScreen = ({ route, navigation }) => {
     setImageToZoomData(data);
     setIsZoomShow(true);
   };
+
+
+  const APIConverter = (response: any) => {
+    function renameKey(obj: any, oldKey: string, newKey: string) {
+      if (obj.hasOwnProperty(oldKey)) {
+        obj[newKey] = obj[oldKey];
+        delete obj[oldKey];
+      }
+    }
+  
+    response.forEach((obj: any) => {
+      renameKey(obj, "product_refno", "productID");
+      renameKey(obj, "product_name", "productName");
+      renameKey(obj, "product_refno", "id");
+      renameKey(obj, "category_refno", "id");
+      renameKey(obj, "category_name", "categoryName");
+      renameKey(obj, "group_refno_name", "activityRoleName");
+      renameKey(obj, "group_refno", "id");
+      renameKey(obj, "group_name", "activityRoleName");
+      renameKey(obj, "service_refno_name", "serviceName");
+      renameKey(obj, "service_refno", "id");
+      renameKey(obj, "service_name", "serviceName");
+      renameKey(obj, "unit_category_names", "unitName");
+      renameKey(obj, "unit_category_name", "unitName");
+      renameKey(obj, "unit_category_refno", "id");
+      renameKey(obj, "unit_name_text", "displayUnit");
+      renameKey(obj, "hsn_sac_code", "hsnsacCode");
+      renameKey(obj, "gst_rate", "gstRate");
+      renameKey(obj, "view_status", "display");
+      renameKey(obj, "product_code", "productCode");
+      renameKey(obj, "unit_display_name", "displayUnit");
+      renameKey(obj, "unit_name", "displayUnit");
+      renameKey(obj, "unitcategoryrefno_unitrefno", "id");
+      renameKey(obj, "with_material_rate", "rateWithMaterials");
+      renameKey(obj, "without_material_rate", "rateWithoutMaterials");
+      renameKey(obj, "short_desc", "shortSpecification");
+      renameKey(obj, "actual_unitname", "selectedUnit");
+      renameKey(obj, "convert_unitname", "convertedUnit");
+      renameKey(obj, "service_product_refno", "productID");
+      renameKey(obj, "locationtype_refno", "id");
+      renameKey(obj, "service_refno_name", "serviceName");
+      renameKey(obj, "locationtype_name", "branchType");
+      renameKey(obj, "workfloor_refno", "id");
+      renameKey(obj, "workfloor_name", "workFloorName");
+      renameKey(obj, "worklocation_refno", "id");
+      renameKey(obj, "worklocation_name", "workLocationName");
+      renameKey(obj, "designtype_refno", "designTypeID");
+      renameKey(obj, "designtype_name", "designTypeName");
+      renameKey(obj, "designtype_image_url", "designImage");
+      renameKey(obj, "materials_setup_refno", "id");
+      renameKey(obj, "service_product_name", "productName");
+      renameKey(obj, "matrails_cost", "materialCost");
+      renameKey(obj, "dealer_product_refno", "productID");
+      renameKey(obj, "brand_refno", "brandID");
+      renameKey(obj, "brand_name", "brandName");
+      renameKey(obj, "company_product_refno", "productID");
+      renameKey(obj, "company_product_price", "price");
+      renameKey(obj, "company_brand_refno", "brandID");
+      renameKey(obj, "company_brand_name", "brandName");
+      renameKey(obj, "design_image_url", "designImage");
+      
+    });
+  
+    return response;
+  };
+
  //#endregion 
  
   return (
