fc79bafe7d06b46a545d6eeefdb081a14a767aeb
Om03 <118772300+samadhanuser5@users.noreply.github.com>
2023-04-12 12:07:12 +0530

    Rate card

--- src/screens/Contractor/RateCard/SendRateCard/sendRateCard.js
+++ src/screens/Contractor/RateCard/SendRateCard/sendRateCard.js
@@ -1,6 +1,27 @@
 import React, { useEffect, useRef } from "react";
-import { ActivityIndicator, View, RefreshControl, LogBox, ScrollView } from "react-native";
-import { FAB, List, Searchbar, Snackbar, Title, Dialog, Portal, Paragraph, Button, Text, TextInput, Card, HelperText, DataTable } from "react-native-paper";
+import {
+  ActivityIndicator,
+  View,
+  RefreshControl,
+  LogBox,
+  ScrollView,
+} from "react-native";
+import {
+  FAB,
+  List,
+  Searchbar,
+  Snackbar,
+  Title,
+  Dialog,
+  Portal,
+  Paragraph,
+  Button,
+  Text,
+  TextInput,
+  Card,
+  HelperText,
+  DataTable,
+} from "react-native-paper";
 import { SwipeListView } from "react-native-swipe-list-view";
 import RBSheet from "react-native-raw-bottom-sheet";
 import Provider from "../../../../api/Provider";
@@ -9,19 +30,25 @@ import NoItems from "../../../../components/NoItems";
 import { theme } from "../../../../theme/apptheme";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { RenderHiddenItems, RenderHiddenMultipleItems } from "../../../../components/ListActions";
+import {
+  RenderHiddenItems,
+  RenderHiddenMultipleItems,
+} from "../../../../components/ListActions";
 import { Styles } from "../../../../styles/styles";
 import { NullOrEmpty } from "../../../../utils/validations";
 import { width } from "@fortawesome/free-solid-svg-icons/faBarsStaggered";
 import { communication } from "../../../../utils/communication";
 // import SearchNAdd from "../../../AddItems/SearchNAdd";
 
-LogBox.ignoreLogs(["Non-serializable values were found in the navigation state"]);
+LogBox.ignoreLogs([
+  "Non-serializable values were found in the navigation state",
+]);
 let userID = 0;
-
+let Sess_company_refno = 0;
+let Sess_branch_refno = 0;
+let Sess_group_refno = 0;
 const SendRateCard = ({ navigation }) => {
-
-   //#region Variables
+  //#region Variables
   const [searchQuery, setSearchQuery] = React.useState("");
   const [isLoading, setIsLoading] = React.useState(true);
   const listData = React.useState([]);
@@ -29,48 +56,58 @@ const SendRateCard = ({ navigation }) => {
   const [refreshing, setRefreshing] = React.useState(false);
   const [snackbarVisible, setSnackbarVisible] = React.useState(false);
   const [snackbarText, setSnackbarText] = React.useState("");
-  const [snackbarColor, setSnackbarColor] = React.useState(theme.colors.success);
-
+  const [snackbarColor, setSnackbarColor] = React.useState(
+    theme.colors.success
+  );
+  const [current, setCurrent] = React.useState({});
   const [clientName, setClientName] = React.useState("");
   const [clientNumber, setClientNumber] = React.useState("");
-  const [unit,setUnit] = React.useState("");
-  const [material,setMaterial]=React.useState("");
-  const [status,setStatus]=React.useState("");
+  const [unit, setUnit] = React.useState("");
+  const [material, setMaterial] = React.useState("");
+  const [status, setStatus] = React.useState("");
   const refRBSheet = useRef();
 
+  //#endregion
 
+  //#region Functions
 
- //#endregion 
-
- //#region Functions
-
- const GetUserID = async () => {
-  const 
-  userData = await AsyncStorage.getItem("user");
-  if (userData !== null) {
-    userID = JSON.parse(userData).UserID;
-    FetchData("");
-  }
-};
+  const GetUserID = async () => {
+    const userData = await AsyncStorage.getItem("user");
+    if (userData !== null) {
+      userID = JSON.parse(userData).UserID;
+      Sess_group_refno = JSON.parse(userData).Sess_group_refno;
+      Sess_branch_refno = JSON.parse(userData).Sess_branch_refno;
+      Sess_company_refno = JSON.parse(userData).Sess_company_refno;
+      FetchData();
+    }
+  };
 
   const FetchData = (from) => {
     if (from === "add" || from === "update") {
-      setSnackbarText("Item " + (from === "add" ? "added" : "updated") + " successfully");
+      setSnackbarText(
+        "Item " + (from === "add" ? "added" : "updated") + " successfully"
+      );
       setSnackbarColor(theme.colors.success);
       setSnackbarVisible(true);
     }
     let params = {
-      AddedByUserID: userID
+      data: {
+        Sess_UserRefno: userID,
+        Sess_company_refno: Sess_company_refno,
+        Sess_branch_refno: Sess_branch_refno,
+        Sess_group_refno: Sess_group_refno,
+        cont_rc_refno: "all",
+      },
     };
 
-    Provider.getAll(`master/getcontractorratecardsentlist?${new URLSearchParams(params)}`)
+    Provider.createDFContractor(
+      Provider.API_URLS.contractor_sendratecard_contrcrefnocheck,
+      params
+    )
       .then((response) => {
         if (response.data && response.data.code === 200) {
           if (response.data.data) {
-            const lisData = [...response.data.data];
-            lisData.map((k, i) => {
-              k.key = (parseInt(i) + 1).toString();
-            });
+            console.log(response.data.data[0]);
             listData[1](response.data.data);
             listSearchData[1](response.data.data);
           }
@@ -103,38 +140,68 @@ const SendRateCard = ({ navigation }) => {
     } else {
       listSearchData[1](
         listData[0].filter((el) => {
-          return el.activityRoleName.toString().toLowerCase().includes(query.toLowerCase());
+          return el.activityRoleName
+            .toString()
+            .toLowerCase()
+            .includes(query.toLowerCase());
         })
       );
     }
   };
 
   const AddCallback = () => {
-    navigation.navigate("AddSendRateCard", { type: "add", fetchData: FetchData });
+    navigation.navigate("AddSendRateCard", {
+      type: "add",
+      fetchData: FetchData,
+    });
   };
 
   const RenderItems = (data) => {
     return (
-      <View style={[Styles.backgroundColor, Styles.borderBottom1, Styles.paddingStart16, Styles.flexJustifyCenter, { height: 80 }]}>
-      <List.Item
-        title={data.item.clientName}
-        titleStyle={{ fontSize: 18 }}
-        description={`Client Number: ${NullOrEmpty(data.item.contactNo) ? "" : data.item.contactNo}\nUnit: ${NullOrEmpty(data.item.unit) ? "" : data.item.unit} `}
-        onPress={() => {
-
-          refRBSheet.current.open(); 
-          setClientName(data.item.clientName);
-          setClientNumber(data.item.contactNo);
-          setUnit(data.item.unit);
-          setMaterial(data.item.inclusiveMaterials ? "Yes": "No");
-          setStatus(data.item.sendStatus ? "Yes": "No");
-
-        }}
-        left={() => <Icon style={{ marginVertical: 12, marginRight: 12 }} size={30} color={theme.colors.textSecondary} name="cards" />}
-        right={() => <Icon style={{ marginVertical: 18, marginRight: 12 }} size={30} color={theme.colors.textSecondary} name="eye" />}
-      />
-
-    </View>
+      <View
+        style={[
+          Styles.backgroundColor,
+          Styles.borderBottom1,
+          Styles.paddingStart16,
+          Styles.flexJustifyCenter,
+          { height: 80 },
+        ]}
+      >
+        <List.Item
+          title={data.item.client_firstname}
+          titleStyle={{ fontSize: 18 }}
+          description={`Client Number: ${
+            NullOrEmpty(data.item.client_mobile_no)
+              ? ""
+              : data.item.client_mobile_no
+          }\nUnit: ${
+            NullOrEmpty(data.item.rc_unit_type_name)
+              ? ""
+              : data.item.rc_unit_type_name
+          } `}
+          onPress={() => {
+            console.log(data.item);
+            setCurrent(data.item);
+            refRBSheet.current.open();
+          }}
+          left={() => (
+            <Icon
+              style={{ marginVertical: 12, marginRight: 12 }}
+              size={30}
+              color={theme.colors.textSecondary}
+              name="cards"
+            />
+          )}
+          right={() => (
+            <Icon
+              style={{ marginVertical: 18, marginRight: 12 }}
+              size={30}
+              color={theme.colors.textSecondary}
+              name="eye"
+            />
+          )}
+        />
+      </View>
     );
   };
 
@@ -144,22 +211,32 @@ const SendRateCard = ({ navigation }) => {
       type: "edit",
       fetchData: FetchData,
       data: {
-        id: data.item.id,
+        ...data.item,
       },
     });
   };
- //#endregion 
- 
+
   return (
     <View style={[Styles.flex1]}>
       <Header navigation={navigation} title="Send Rate Card" />
       {isLoading ? (
-        <View style={[Styles.flex1, Styles.flexJustifyCenter, Styles.flexAlignCenter]}>
+        <View
+          style={[
+            Styles.flex1,
+            Styles.flexJustifyCenter,
+            Styles.flexAlignCenter,
+          ]}
+        >
           <ActivityIndicator size="large" color={theme.colors.primary} />
         </View>
       ) : listData[0].length > 0 ? (
         <View style={[Styles.flex1, Styles.flexColumn, Styles.backgroundColor]}>
-          <Searchbar style={[Styles.margin16]} placeholder="Search" onChangeText={onChangeSearch} value={searchQuery} />
+          <Searchbar
+            style={[Styles.margin16]}
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
           <SwipeListView
             previewDuration={1000}
             previewOpenValue={-72}
@@ -179,25 +256,87 @@ const SendRateCard = ({ navigation }) => {
             disableRightSwipe={true}
             rightOpenValue={-72}
             renderItem={(data) => RenderItems(data)}
-            renderHiddenItem={(data, rowMap) => RenderHiddenItems(data, rowMap, [EditCallback])}
+            renderHiddenItem={(data, rowMap) =>
+              RenderHiddenItems(data, rowMap, [EditCallback])
+            }
           />
         </View>
       ) : (
-        <NoItems icon="format-list-bulleted" text="No records found. Add records by clicking on plus icon." />
+        <NoItems
+          icon="format-list-bulleted"
+          text="No records found. Add records by clicking on plus icon."
+        />
       )}
       <FAB style={[Styles.fabStyle]} icon="plus" onPress={AddCallback} />
-      <Snackbar visible={snackbarVisible} onDismiss={() => setSnackbarVisible(false)} duration={3000} style={{ backgroundColor: snackbarColor }}>
+      <Snackbar
+        visible={snackbarVisible}
+        onDismiss={() => setSnackbarVisible(false)}
+        duration={3000}
+        style={{ backgroundColor: snackbarColor }}
+      >
         {snackbarText}
       </Snackbar>
 
-      <RBSheet ref={refRBSheet} closeOnDragDown={true} closeOnPressMask={true} dragFromTopOnly={true} height={380} animationType="fade" customStyles={{ wrapper: { backgroundColor: "rgba(0,0,0,0.5)" }, draggableIcon: { backgroundColor: "#000" } }}>
+      <RBSheet
+        ref={refRBSheet}
+        closeOnDragDown={true}
+        closeOnPressMask={true}
+        dragFromTopOnly={true}
+        height={500}
+        animationType="fade"
+        customStyles={{
+          wrapper: { backgroundColor: "rgba(0,0,0,0.5)" },
+          draggableIcon: { backgroundColor: "#000" },
+        }}
+      >
         <View>
-          <Title style={[Styles.paddingHorizontal16]}>{clientName}</Title>
+          <Title style={[Styles.paddingHorizontal16]}>
+            {current.client_firstname}
+          </Title>
           <ScrollView style={{ marginBottom: 64 }}>
-            <List.Item title="Client Number" description={clientNumber} />
-            <List.Item title="Unit" description={unit} />
-            <List.Item title="Material" description={material} />
-            <List.Item title="Status" description={status} />
+            <List.Item
+              title="Client Number"
+              description={current.client_mobile_no}
+            />
+            <List.Item title="Unit" description={current.rc_unit_type_name} />
+            <List.Item title="Material" description={current.material_status} />
+            <List.Item title="Status" description={current.rc_status} />
+            {current?.action_button?.includes("Preview Rate Card") && (
+              <View style={{ alignItems: "center", marginTop: "4%" }}>
+                <Button
+                  mode="outlined"
+                  style={{
+                    borderColor: "pink",
+                    borderWidth: 1.2,
+                    color: "pink",
+                    width: "80%",
+                  }}
+                  // onPress={() => {
+                  //   refRBSheet.current.close();
+                  // }}
+                >
+                  <Text style={{ color: "pink" }}>Preview Rate Card</Text>
+                </Button>
+              </View>
+            )}
+            {current?.action_button?.includes("Send Rate Card") && (
+              <View style={{ alignItems: "center", marginTop: "4%" }}>
+                <Button
+                  mode="outlined"
+                  style={{
+                    borderColor: "green",
+                    borderWidth: 1.2,
+                    color: "green",
+                    width: "80%",
+                  }}
+                  // onPress={() => {
+                  //   refRBSheet.current.close();
+                  // }}
+                >
+                  <Text style={{ color: "green" }}>Send Rate Card</Text>
+                </Button>
+              </View>
+            )}
           </ScrollView>
         </View>
       </RBSheet>
